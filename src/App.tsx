bd032885f5a5a3dba8488b4cfec9209137beee26
Ranjith R <ranjithtweaks@gmail.com>
2025-10-28 17:40:08 +0530

    feat(storkstone): add prerendered storkstone.html, SEO meta (canonical, OG, Twitter), and Storkstone page/route

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { Navigation } from './components/Navigation';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Projects } from './components/Projects';
-import { Experience } from './components/Experience';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import { ScrollToTop } from './components/ScrollToTop';
-import { Storkstone }  from './components/Storkstone';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import StorkstonePage from './pages/StorkstonePage';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Navigation />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Experience />
-        <Storkstone />
-        <Contact />
-      </main>
-      <Footer />
-      <ScrollToTop />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/storkstone" element={<StorkstonePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
